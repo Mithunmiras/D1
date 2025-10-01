eba450e0d3479ad961de92e57c37450d0ee1e434
mitzz <mitzzftw@gmail.com>
2025-10-01 19:57:00 +0530

    Updated index.jsx

--- src/Pages/Public/services/index.jsx
+++ src/Pages/Public/services/index.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { motion } from "framer-motion";
-import { pageTransition } from "../../../utils/PageTransition";
 import ServicesHero from "./components/ServicesHero";
 import CoreServices from "./components/CoreServices";
 import SaaSProducts from "./components/SaaSProducts";
@@ -10,16 +8,13 @@ import { servicePageData } from "../../../config/servicesPageData";
 
 const Services = () => {
   return (
-    <motion.div
-      {...pageTransition}
-      className="min-h-screen"
-    >
+    <div className="min-h-screen">
       <ServicesHero data={servicePageData.hero} />
       <CoreServices data={servicePageData.coreServices} />
       <SaaSProducts data={servicePageData.saasProducts} />
       <ProcessSection data={servicePageData.process} />
       <CTABanner data={servicePageData.cta} />
-    </motion.div>
+    </div>
   );
 };
 
